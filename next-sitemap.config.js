1e33c9cca00de38e1251132297b4c393ef2b549d
victor-kiss <victorkiss10@gmail.com>
2022-12-19 15:11:24 -0300

    changes in frequence of sitemap (next-sitemap)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,5 @@ module.exports = {
     siteUrl: 'https://3dplace.vercel.app/',
     generateRobotsTxt: true, // opcional
     priority: 1,
-    changefreq: "daily",
+    changefreq: true,
   }
--- next-sitemap.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    siteUrl: 'https://3dplace.vercel.app/',
-    generateRobotsTxt: true, // opcional
-    priority: 1,
-    changefreq: "daily",
-  }
